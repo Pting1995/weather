df5e94d982db84931b6092b11f10aa1968cb92a0
Peter Ting <pting1995@gmail.com>
2021-01-04 23:07:45 -0800

    changed html elements, and added comments

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // container1
 //     search for a city
 //     list cities from local storage
+
+// container2
+//         display city name, date and picture of weather
+//         temp
+//         humidity
+//         windspeed
+//         UV index with changing color background depending on how high UV Index is
+//         5 day forecast
+//             date and pic of weather
+//             temp
+//             humidity
