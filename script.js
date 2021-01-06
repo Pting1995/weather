157d1160a15985751d3570503c44fa422492fdb7
Peter Ting <pting1995@gmail.com>
2021-01-05 21:56:32 -0800

    added local storage for city history

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+var cityName = $("#city-name");
+var cityHistory = $(".city-history")
+var cityArr = []
+
+$("#submit-button").on("click", function(event) {
+    event.preventDefault();
+
+    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + cityName.val() + "&appid=3d8a9db33d6081c4a896f43ab9165e4d";
+    // var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London&appid=3d8a9db33d6081c4a896f43ab9165e4d";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        console.log(queryURL);
+        addHistory(cityName.val())
+    });
+    
+});
+
+function addHistory(cityToSave) {
+    cityArr.push(cityToSave)
+    localStorage.setItem("cityHistory", JSON.stringify(cityArr))
+    cityHistory.text(JSON.parse(localStorage.getItem("cityHistory")))
+}
+
+cityHistory.text(JSON.parse(localStorage.getItem("cityHistory")))
+
+    // var p = $("<p>")
+    // p.text(localStorage.getItem("cityHistory"))
+    // cityHistory.append(p)
+
+
 // container1
 //     search for a city
 //     list cities from local storage
